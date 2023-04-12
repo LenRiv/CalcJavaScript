b580e0bede2d107b4ab6cc39f7850125b74cae0c
LenRiv <lorenarivasdev@gmail.com>
2023-04-12 19:27:31 +0200

    complete project

---
+++ README.md
@@ -0,0 +1,6 @@
+Este proyecto es una calculadora web que permite realizar operaciones matemáticas básicas, como suma, resta, multiplicación y división, de manera interactiva. 
+El código HTML define la estructura de la página web y utiliza diferentes etiquetas para organizar y mostrar los distintos elementos de la interfaz de usuario. 
+El código CSS proporciona los estilos visuales para los elementos HTML, como el fondo, el color de texto, la fuente y la disposición de los botones, lo que contribuye a la experiencia de usuario.
+El archivo "main.js",contiene el código JavaScript que define el comportamiento de la calculadora, como la adición de números, la eliminación de caracteres, el manejo de errores y la evaluación de expresiones matemáticas complejas.
+En general, la combinación de estas tecnologías web permite crear una herramienta útil y accesible para realizar cálculos matemáticos de manera rápida y sencilla.
+Esta calculadora web también cuenta con un modo oscuro o "dark mode".Cuya funcionalidad sigue siendo la misma que en el modo claro.
---
+++ main.js
@@ -0,0 +1,54 @@
+
+//Seleccionar elementos HTML.//
+
+//Visor de la calculadora
+const display = document.querySelector("#box");
+//Seleccionar todos los botones
+const allButtons = document.querySelectorAll("button");
+//Interruptor tema oscuro
+const activeSwitch = document.querySelector(".toggle__switch");
+//Contenedor principal 
+const calculator = document.querySelector(".calculator");
+
+
+//Dar funcionalidad a la calculadora//
+
+//Recorrer todos los botones 
+allButtons.forEach((item) => {
+  item.onclick = () => {
+
+    //Limpiar la pantalla
+    if (item.id == "clear") {
+      display.innerText = "";
+
+    //Condicion para el control del boton de borrado uno a uno
+    } else if (item.id == "back") {
+      let string = display.innerText.toString();
+      display.innerText = string.substring(0, string.length - 1);
+
+    //Metodo eval() para el control de operaciones
+    } else if (display.innerText != "" && item.id == "equal") {
+      display.innerText = eval(display.innerText);
+
+    //Para controlar errores del boton
+    } else if (display.innerText == "" && item.id == "equal") {
+      display.innerText = "Null";
+      setTimeout(() => (display.innerText = ""), 1000);
+
+    } else {
+    //Añadir interaccion de botones
+      display.innerText += item.id;
+    }
+  };
+});
+
+
+//Modo oscuro
+let isDark = true;//Empieza en true
+
+activeSwitch.onclick = () => {
+  calculator.classList.toggle("dark");//Inicia todo el tema dark
+  activeSwitch.classList.toggle("active");//Estado inicial
+  isDark = !isDark;//Cambia a false y vuelve al estado inicial
+
+};
